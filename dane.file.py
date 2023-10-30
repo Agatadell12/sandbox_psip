88a1e06b97a992837a08192cc8bcfabf035e1eff
Agatadell12 <149382123+Agatadell12@users.noreply.github.com>
2023-10-30 14:45:35 +0100

    Add fuctionale

---
+++ dane.file.py
@@ -0,0 +1,10 @@
+ users_list: list = [
+    {"name":"agata","nick":"mjr","posts":0},
+{"name":"mateusz","nick":"świetlik","posts":2000},
+    {"name":"Bartosz","nick":"Baran","posts":31245},
+    {"name":"Kamil","nick":"KOC","posts":332},
+    {"name":"Marek","nick":"wiss","posts":0},
+    {"name":"Wiktoria","nick":"witka","posts":500},
+    {"name":"Wiktoria","nick":"Mona","posts":44}
+    ]
+
