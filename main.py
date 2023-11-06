8b464e952754754d08dd314d4b1daf9fbe251d2a
Agatadell12 <149382123+Agatadell12@users.noreply.github.com>
2023-11-06 12:56:32 +0100

    chore: add functionaly

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from dane import users_list
 
+
+
+def add_users_to(users_list: list) -> None:
+    """
+    add object to list
+    :param users_list: lista - user list
+    :return: none
+    """
+
+    name = input('podaj imie!')
+    posts =  input('podaj liczbę postów!')
+    users_list.append({'name':name,'posts': posts})
+
+add_users_to(users_list)
+add_users_to(users_list)
+add_users_to(users_list)
+
+
 for user in users_list:
     print(f' Twój znajomy {user["name"]} dodał {user ["posts"]}')
 
