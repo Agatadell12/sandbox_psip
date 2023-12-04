64316fa49a2b5df09d8ee8eeba08c14f3bfc0d63
Agatadell12 <149382123+Agatadell12@users.noreply.github.com>
2023-12-04 15:47:17 +0100

    wip: crud dla bazy danych
    Co-Authored-By: Katarzyna09-c <149382224+katarzyna09-c@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from my_fanctiones import gui
 
 gui(users_list)
 
+#  TODO dodac tabele do bazy danych reprezentujaca uzytkownika
+#  TODO wlaczyc do aktualnego kodu baze danych
+
+#  TODO napisac klase uzytkonika o struktorze zgodnej z ta zdefiniowana w my_data
+
+#  TODO oddac sprawozdanie z powyrzszego do dnia 20.12.2023 sroda
+# TODO spraozdanie ma zaweirac strone tytulowa, spis tresci, opis kodu realizujacego zadnie rysowania mapy uwzgledniajacy poszczegilne funkcjie i cih skladnie, podsumowanie i wnioski koncowe(screeny i zdjecia -spoko).
+# TODO zrealizowany kod na githubie
--- notatki.py
+++ notatki.py
@@ -1,21 +1,49 @@
-import requests
+import sqlalchemy
+
+db_params = sqlalchemy.URL.create(
+    drivername="postgresql+psycopg2",
+    username="postgres",
+    password="Psip_2023",
+    host="localhost",
+    database="postgres",
+    port=5432
+)
 
 
-class User:
-    def __init__(self, miasto):
-        self.miasto = miasto
+engine = sqlalchemy.create_engine(db_params)
+connection = engine.connect()
+#sql_query_1 = aglalchemy.text("INSERT INTO public.my_table( name) VALUES('kepa');")
+#sql_query_1 = sqlalchemy.text("select * from public.my_table;")
+#user=input('podaj nazwe zawodnika do usuniecia')
+#sql_query_1 = sqlalchemy.text("DELETE FROM public.my_table WHERE name='{user}';")
+# kogo_zamienic=input('podaj kogo zmienic')
+# na_kogo=input('podaj na kogo zamienic')
+# sql_query_1 = sqlalchemy.text("UPDATE public.my_table SET name= {'na_kogo'} WHERE name= {'kogo_zamienic'};")
 
-    def pogoda_z(self, miasto: str):
-        url = f'https://danepubliczne.imgw.pl/api/data/synop/station/{miasto}'
-        return requests.get(url).json()
+def dodaj_uzytkownika(user: str):
+    sql_query_1 = sqlalchemy.text("INSERT INTO public.my_table( name) VALUES('{user}');")
+    connection.execute(sql_query_1)
+    connection.commit()
 
+# cwok='staniu'
+# dodaj_uzytkownika(cwok)
 
-npc_1 = User(miasto="warszawa")
-npc_2 = User(miasto="zamosc")
-print(npc_1.miasto)
-print(npc_2.miasto)
+def usun_uzytkownika(user: str):
+    sql_query_1 = sqlalchemy.text("DELETE FROM public.my_table WHERE name='{user}';")
+    connection.execute(sql_query_1)
+    connection.commit()
 
-print(npc_1.pogoda_z(npc_1.miasto))
-print(npc_2.pogoda_z(npc_1.miasto))
+# cwok='staniu'
+# usun_uzytkownika(cwok)
 
-pogoda_z("warszawa")
+def aktualizuj_uzytkonika(user_1: str, user_2: str):
+    sql_query_1 = sqlalchemy.text("UPDATE public.my_table SET name= {'user_1'} WHERE name= {'user_2'};")
+    connection.execute(sql_query_1)
+    connection.commit()
+
+aktualizuj_uzytkonika(
+    user_1=input('kogo zaminic'),
+    user_2=input('na kogo zmienic'))
+
+# connection.execute(sql_query_1)
+# connection.commit()
